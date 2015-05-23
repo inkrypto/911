de511d27062d0241aeccf450b4784ac154a006d3
brent morris <inkrypto@gmail.com>
2015-05-23 16:52:04 -0400

    small changes in format

--- 911.sh
+++ 911.sh
@@ -4,24 +4,23 @@ strings -a - $BIN > binary_strings.txt
 
 FILE=binary_strings.txt
 
-#Binary Protection - ARC (vulnerable if absent)
+echo "Checking for vulnerabilities"
+echo "1. Binary Protection ARC (vulnerable if absent)"
 arc=$(strings -a - < $FILE |grep "_objc_release\|_objc_autoreleaseReturnValue\|_objc_retain")
   if [ "$arc" ]
-    then
-      echo "" 
-      echo "Application is using ARC protection."
+    then 
+      echo "Application is using ARC protection. Not vulnerable"
+      echo ""
     else
-      echo "" 
       echo "VULN NAME: ARC"
       echo "The application does not use the provided iOS memory management framework, ARC. ARC stands for Automatic Reference Counting and builds in intelligent \"Garbage Collection\" and memory deallocations. While not using ARC is not a vulnerability per se, it is highly recommended. Manual memory management in iOS requires a lot of overhead in Objective-C and if not done right can lead to performance losses and security vulnerabilities."
       echo ""
   fi
 
-#Bluetooth (vulnerable if present)
+echo "2. Bluetooth (vulnerable if present)"
 bluetooth=$(grep "GKSession\|MCSession\|CBCentralManager" $FILE)
   if [ "$bluetooth" ]
     then
-      echo "" 
       echo "VULN NAME: Bluetooth"
       echo "The binary contains references to either GKSession or the  Multipeer Connection or Core Bluetooth frameworks, which provide Bluetooth capabilities to the application."
       echo "" 
@@ -33,11 +32,10 @@ bluetooth=$(grep "GKSession\|MCSession\|CBCentralManager" $FILE)
       echo ""
   fi
 
-#Calendar (vulnerable if present)
+echo "3. Calendar (vulnerable if present)"
 calendar=$(grep "EKEventStore\|EKCalendar" $FILE)
   if [ "$calendar" ]
     then
-      echo "" 
       echo "VULN NAME: Calendar"
       echo "The binary contains references to the EKEventStore class, which represents the calendar database."
       echo "" 
@@ -49,14 +47,13 @@ calendar=$(grep "EKEventStore\|EKCalendar" $FILE)
       echo ""
   fi
 
-#Weak Crypto (vulnerable if present)
+echo "4. Weak Crypto (vulnerable if present)"
 crypto=$(grep "CC_MD2\|CC_MD4\|CC_MD5\|CC_SHA1\|kCCAlgorithmDES" "$FILE")
   if [ "$crypto" ] 
     then
-      echo "" 
       echo "VULN NAME: Weak Crypto"
       echo "The app is using the following deprecated crypto:"
-      echo "VULN LOCATIONS: "
+      echo "VULN Crypto: "
       echo "$crypto/n"
       echo "" 
     else
@@ -64,26 +61,25 @@ crypto=$(grep "CC_MD2\|CC_MD4\|CC_MD5\|CC_SHA1\|kCCAlgorithmDES" "$FILE")
       echo "" 
   fi
 
-#Data Protection API Use (vulnerable if absent)
+echo "5. Data Protection API Use (vulnerable if absent)"
 dpi=$(strings -a - < $FILE |grep “NSDataWritingFileProtectionComplete$\|NSFileProtectionComplete$\|NSDataWritingFileProtectionCompleteUnlessOpen$\|NSFileProtectionCompleteUnlessOpen$\|NSDataWritingFileProtectionCompleteUntilFirstUserAuthentication$\|NSFileProtectionCompleteUntilFirstUserAuthentication$”)
   if [ -n "$dpi"] 
     then
-      echo ""
       echo "VULN NAME: Data Protection API Not Used" 
       echo "The application does not protect on-disk data with the iOS Data Protection API. The Data Protection API can be used in iOS 4 and later to use the built-in encryption hardware on the device to encrypt files on disk."
       echo "" 
-      echo "VULN LOCATIONS: "
-      echo "$dpi\n"
+      echo "VULN FILES: "
+      echo "$dpi\n" #This not working like it is in Mobius
       echo "" 
     else
       echo "Application uses the Data protection API"
+      echo ""
   fi
 
-#Geolocation (vulnerable if present)
+echo "6. Geolocation (vulnerable if present)"
 loc=$(grep -i "cllocation" $FILE)
   if [ "$loc" ]
     then
-      echo "" 
       echo "VULN NAME: Geolocation"
       echo "The application uses geolocation data. This data, while valuable for some functions of mobile applications, can also constitute a privacy leak should the application store or send it unencrypted or to a 3rd party not authorized by the phone's owner."
       echo "" 
@@ -91,16 +87,14 @@ loc=$(grep -i "cllocation" $FILE)
       echo "$loc\n"
       echo "" 
     else
-      echo "" 
-      echo "no GEO alert"
+      echo "no GEO used. Not vulnerable."
       echo "" 
   fi  
 
-#Jailbreak Detection (vulnerable if absent)
+echo "7. Jailbreak Detection (vulnerable if absent)"
 jail=$(strings -a - < $FILE |grep "^/bin/bash$\|^/Applications/Cydia.app$\|/cydia.log$\|cydia://")
   if [ -n "$jail" ] 
     then
-      echo "" 
       echo "Jailbreak Proection:"
       echo ""  
       echo "$jail\n"
@@ -111,11 +105,10 @@ jail=$(strings -a - < $FILE |grep "^/bin/bash$\|^/Applications/Cydia.app$\|/cydi
       echo "" 
   fi
 
-#Shared Keychain (vulnerable if present)
+echo "8. Shared Keychain (vulnerable if present)"
 keychain=$(grep "_kSecAttrAccessGroup$" $FILE)
   if [ "$keychain" ]
     then
-      echo "" 
       echo "VULN NAME: Shared Keychain"
       echo "The binary contains references to kSecAttrAccessGroup, which is used to set up shared keychain access."
       echo "" 
@@ -127,11 +120,10 @@ keychain=$(grep "_kSecAttrAccessGroup$" $FILE)
       echo "" 
   fi
 
-#Logging (vulnerable if present)
+echo "9. Logging (vulnerable if present)"
 nslog=$(grep "_NSLog$" $FILE)
   if [ "$nslog" ]
-    then
-      echo "" 
+    then 
       echo "VULN NAME: Logging"
       echo "The application has logging enabled. Ensure this logging doesn't reveal sensitive data. It is good practice to only use logging in the DEBUG/Development phases of design."
       echo "" 
@@ -143,11 +135,10 @@ nslog=$(grep "_NSLog$" $FILE)
       echo "" 
   fi
 
-#Pasteboard (vulnerable if present)
+echo "10. Pasteboard (vulnerable if present)"
 pboard=$(grep -i "generalpasteboard" $FILE)
   if [ "$pboard" ]
   then
-      echo "" 
       echo "VULN NAME: Pasteboard"
       echo "The binary contains references to the generalPasteboard method."
       echo "" 
@@ -159,11 +150,10 @@ pboard=$(grep -i "generalpasteboard" $FILE)
       echo "" 
   fi
 
-#Binary Protection – Path Disclosure (vulnerable if present) need unecrypted
+echo "11. Binary Protection Path Disclosure (vulnerable if present) need unecrypted"
 paths=$(strings -a - $FILE |grep "/Users/" |cut –d/ -f3| sort –u) #this one needs an unnecrypted binary
   if [[ -n $paths ]]
     then
-      echo ""
       echo "VULN NAME: Path Disclosure" 
       echo "This application includes file paths to source code files in debug information stored within the app's executable. These file paths often include usernames or other information related to the developer of the app. This information could be used to assist in targeting the app developer or development company. Below are user names parsed from the application binary:"
       echo "" 
@@ -185,7 +175,7 @@ paths=$(strings -a - $FILE |grep "/Users/" |cut –d/ -f3| sort –u) #this one 
       # sabilrahim
       # vduggal
 
-#Unencrypted Photo Storage (vulnerable if present)
+echo "12. Unencrypted Photo Storage (vulnerable if present)"
 photostore=$(grep "UIImageWriteToSavedPhotosAlbum\|sharedPhotoLibrary" $FILE)
   if [ "$photostore" ]
     then
@@ -200,11 +190,10 @@ photostore=$(grep "UIImageWriteToSavedPhotosAlbum\|sharedPhotoLibrary" $FILE)
       echo "" 
   fi
 
-#Binary Protection – PIE (vulnerable if PIE is absent)
+echo "13 Binary Protection PIE (vulnerable if PIE is absent)"
 pie=$(otool -hvm $BIN)
   if [ "$pie" ] 
     then
-      echo "" 
       echo "The application is using PIE:"
       echo "" 
       echo "LOCATION: " 
@@ -216,12 +205,11 @@ pie=$(otool -hvm $BIN)
       echo "" 
   fi
 
-#Push Notifications (vulnerable if present)
+echo "14. Push Notifications (vulnerable if present)"
 pushnotreg=$(grep -i "registerForRemoteNotificationTypes\|registerForRemoteNotifications" $FILE)
 pushnottok=$(grep -i "didRegisterForRemoteNotificationsWithDeviceToken" $FILE)
   if [ "$pushnotreg" ] && [ "$pushnottok" ]
-    then
-      echo "" 
+    then 
       echo "VULN NAME: "
       echo "This app is using Push Notifications. The binary contains references to methods used to register a device for push notifications, registerForRemoteNotificationTypes and didRegisterForRemoteNotificationsWithDeviceToken."
       echo ""
@@ -231,13 +219,13 @@ pushnottok=$(grep -i "didRegisterForRemoteNotificationsWithDeviceToken" $FILE)
       echo "" 
     else
       echo "noalert for push notifications"
+      echo ""
     fi
 
-#Client-Side SQL Injection (vulnerable if present)
+echo "15. Client-Side SQL Injection (vulnerable if present)"
 sql=$(strings -a - < $FILE |grep -i "^begin transaction\|^select .* from \|^update .* set \|^delete from \|^insert into " | grep "%@" | grep -v "SELECT id,access_token FROM test_account WHERE app_id")
   if [ "$sql" ]
     then
-      echo "" 
       echo "VULN NAME: SQL Injection"
       echo "The inspected application uses SQL queries that are populated with dynamic input via format strings. If the dynamic input is derived from untrusted sources such as user input from the querystring, form fields, or HTTP headers, then SQL injection may be possible. To protect against SQL injection attacks, use parameterized queries whenever possible, in which inputs are bound to specific query parameters. The following statements containing format strings were detected:"
       echo ""
@@ -249,11 +237,10 @@ sql=$(strings -a - < $FILE |grep -i "^begin transaction\|^select .* from \|^upda
      echo "" 
   fi
 
-#sqlite3_prepare (vulnerable if present) 
+echo "16. sqlite3_prepare (vulnerable if present)" 
 sqlite3=$(grep "sqlite3_prepare$" $FILE)
   if [ "$sqlite3" ]
     then
-      echo "" 
       echo "VULN NAME: SQLite3"
       echo "The applications uses the 'sqlite3_prepare' function for database calls. This function has been deprecated and is known to contain security weaknesses. Use 'sqlite3_prepare_v2' as an alternative."
       echo ""
@@ -265,11 +252,10 @@ sqlite3=$(grep "sqlite3_prepare$" $FILE)
       echo "" 
   fi
 
-#Binary Protection – Stack Protection (vulnerable if absent)
+echo "17. Binary Protection – Stack Protection (vulnerable if absent)"
 stack=$(otool -Ivm $BIN |grep stack_chk)
   if [ "$stack" ] 
     then
-      echo "" 
       echo "Stack Protection Present:"
       echo ""
       echo "LOCATION: " 
@@ -281,11 +267,10 @@ stack=$(otool -Ivm $BIN |grep stack_chk)
       echo "" 
   fi
 
-#Shared Directories (vulnerable if present)
+echo "18. Shared Directories (vulnerable if present)"
 tmpdir=$(strings -a - < $FILE |grep "^/private/var/tmp\|^/var/tmp" | grep -v "\.m\|xcode")
   if [ "$tmpdir" ]
     then
-      echo ""
       echo "VULN NAME: Shared Temp Directory" 
       echo "The application accesses a shared directory, such as '/tmp'. If application files are created in a shared directory, anyone else with access to those files can modify or replace them. If temporary files are necessary, ensure they are stored in a directory that is restricted to the application, such as <Application_Home>/tmp/."
       echo "" 
@@ -297,11 +282,10 @@ tmpdir=$(strings -a - < $FILE |grep "^/private/var/tmp\|^/var/tmp" | grep -v "\.
     echo "" 
   fi
 
-#UDID transfer, including over HTTPS (vulnerable if present)
+echo "19. UDID transfer, including over HTTPS (vulnerable if present)"
 udid=$(strings -a - < $FILE |grep ://| grep -i "udid")
   if [ -n "$dir" ]
     then
-      echo ""
       echo "VULN NAME: UDID discovered" 
       echo "The Application is transfer the UDID as a parameter"
       echo ""
@@ -312,27 +296,24 @@ udid=$(strings -a - < $FILE |grep ://| grep -i "udid")
       echo "" 
   fi
 
-#Unencrypted URLs
+echo "20. Unencrypted URLs"
 urls=$(grep -oE "\bhttp://[^<>[:space:]]*[^\"[:space:]<>']" $FILE |grep -v "^http://$\| http\|www.apple.com/appleca\|www.apple.com/DTDs\|phobos.apple.com/WebObjects/MZStore.woa/wa/viewSoftware\|ns.adobe.com"|sort -u)
   if [ -n "$urls" ]
     then
-      echo ""
       echo "No Unencrypted URLs found"
       echo "" 
 
     else
-      echo ""
       echo "VULN NAME: Unencrypted URLs" 
       echo "VULN LOCATIONS: " 
       echo "$urls\n"
       echo ""
   fi
                                 
-#URL Schemes (vulnerable if present)
+echo "21. URL Schemes (vulnerable if present)"
 url_schemes=$(strings -a - $FILE |grep -oE "[a-zA-Z][a-zA-Z0-9\+\-\.]*://[^[:space:]\<\>\#\"\']+"|grep -v "http://\|https://\|radr://"| sort –u)
   if [ "url_schemes" ]
     then
-      echo "" 
       echo "VULN NAME: URL Schemes"
       echo "The application uses custom URL Schemes (protocol handlers) to communicate with other iOS apps on the phone. When registering a custom URL scheme it is important to apply input validation to that receiver. The use of regular expressions to whitelist input characters and maximum length from other applications is recommended."
       echo ""
@@ -343,4 +324,3 @@ url_schemes=$(strings -a - $FILE |grep -oE "[a-zA-Z][a-zA-Z0-9\+\-\.]*://[^[:spa
       echo "clear"
       echo "" 
   fi
-
